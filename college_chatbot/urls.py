68f1a7eda680bf4a6ff636300b312410544512e7
Apurva Lotake <aplotake@gmai.com>
2025-09-16 00:18:11 +0530

    Initial commit of Django chatbot

---
+++ college_chatbot/urls.py
@@ -0,0 +1,33 @@
+"""
+URL configuration for college_chatbot project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path
+from chatbot import views  # make sure 'chatbot' matches your app folder name
+from chatbot.views import home, get_response  # adjust as needed
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('get_response/', views.get_response, name='get_response'),  # make sure this view exists
+    path('', views.home, name='home'),  # Optional: chatbot homepage
+]
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
